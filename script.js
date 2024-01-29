2c8bd5140b7bc0f4d725d0aa290e3aaa75ecbf5f
Rsmohanraj <154896181+Rsmohanraj@users.noreply.github.com>
2024-01-29 22:52:41 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,33 @@
+setTimeout(() => {
+    document.getElementById("augest").innerText="10"
+    setTimeout(() => {
+      document.getElementById("augest").innerText="9"
+      setTimeout(() => {
+        document.getElementById("augest").innerText="8"
+        setTimeout(() => {
+          document.getElementById("augest").innerText="7"
+          setTimeout(() => {
+            document.getElementById("augest").innerText="6"
+            setTimeout(() => {
+              document.getElementById("augest").innerText="5"
+              setTimeout(() => {
+                document.getElementById("augest").innerText="4"
+                setTimeout(() => {
+                  document.getElementById("augest").innerText="3"
+                  setTimeout(() => {
+                    document.getElementById("augest").innerText="2"
+                    setTimeout(() => {
+                      document.getElementById("augest").innerText="1"
+                      setTimeout(() => {
+                        document.getElementById("augest").innerHTML="Happy Independence Day!!!"
+                      }, 1000);
+                    }, 1000);
+                  }, 1000);
+                }, 1000);
+              }, 1000);
+            }, 1000);
+          }, 1000);
+        }, 1000);
+      }, 1000);
+    }, 1000);
+  }, 1000);
